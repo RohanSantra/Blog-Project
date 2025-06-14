1ab1c43e499c7a455ab8698a441d17acbe1c5c12
Rohan <kajalsantra816@gmail.com>
2025-06-14 22:01:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/blog-project/',
+  base:'/Blog-Project/',
   plugins: [react(), tailwindcss()],
 })
